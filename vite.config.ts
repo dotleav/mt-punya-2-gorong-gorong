e8a10e43f94328b9b3531e9d39521dea49842578
dotleav <whyutakethisusername@gmail.com>
2026-06-01 17:55:44 +0700

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import viteReact from '@vitejs/plugin-react'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    viteTsConfigPaths({ projects: ['./tsconfig.json'] }),
+    tailwindcss(),
+    viteReact(),
+  ],
+  base: './',
+})
